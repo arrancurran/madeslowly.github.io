d9b10111f7fc85da906f3d0ebe08ee1db7aeffc1
arrancurran <arrancurran@gmail.com>
2021-12-20 23:24:48 +0000

    split

--- assets/js/displayItem.js
+++
@@ -1,9 +0,0 @@
-// this gets called on mobile devices for the catogories menu
-
-function displayItem( itemID ) {
-  var item = document.getElementById( itemID );
-  if (item === undefined) {
-    item = 'dropdownContent' ;
-  } ;
-  item.classList.toggle('display-none');
-}
